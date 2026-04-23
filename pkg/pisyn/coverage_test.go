afa2d8dad07660559b12054ec3917741ce887df9
t0st <16083626+t0st@users.noreply.github.com>
2026-04-23 07:31:44 +0200

    fix: remove unused test funcs

--- pkg/pisyn/coverage_test.go
+++ pkg/pisyn/coverage_test.go
@@ -188,16 +188,3 @@ func TestConstructAccessors(t *testing.T) {
 		t.Error("app node should be self")
 	}
 }
-
-func contains(s, sub string) bool {
-	return len(s) >= len(sub) && (s == sub || len(s) > 0 && containsStr(s, sub))
-}
-
-func containsStr(s, sub string) bool {
-	for i := 0; i <= len(s)-len(sub); i++ {
-		if s[i:i+len(sub)] == sub {
-			return true
-		}
-	}
-	return false
-}
